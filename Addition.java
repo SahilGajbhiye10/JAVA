c2958d1dac3ffd3c53932279d98a737bb06dd127
SahilGajbhiye10 <127387841+SahilGajbhiye10@users.noreply.github.com>
2023-03-09 15:22:38 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,9 @@
+class MyJava
+{
+	public static void main(String[] args)
+	{
+	 int a=10,b=20;
+	 int res=a+b;
+	 System.out.print("Addition is "+res);
+	}
+}
---
+++ Substraction.java
@@ -0,0 +1,7 @@
+class Sub{
+	public static void main(String[] args) {
+		int a=10,b=5;
+		int res=a-b;
+		System.out.print("Subtraction is "+res);
+			}
+}
