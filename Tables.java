90bf95120ea542bf70ebe7a49f426a910dfbdd63
SahilGajbhiye10 <127387841+SahilGajbhiye10@users.noreply.github.com>
2023-03-13 20:32:41 +0530

    Add files via upload

---
+++ Tables.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class MyJava {
+
+public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+
+System.out.println("Enter no: "); int n=sc.nextInt();
+
+for (int i=0;i<=10 ; ++i) {
+
+System.out.println(n+" * "+i+" = "+(n*i));
+  }
+ }
+}                  
